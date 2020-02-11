53417c3dc1e0895b653515f4c85612b3ad2a62f1
srivishnu2101-1 <srivishnu.s1@thoughtworks.com>
2020-02-11 14:43:40 +0530

    added | not equal game object test

--- src/test/java/GameObjectTest.java
+++ src/test/java/GameObjectTest.java
@@ -1,20 +1,28 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class GameObjectTest {
 
     @Test
-    void shouldCheckTwoGameObjectsAreEqual() {
+    void shouldCheckTwoGameObjectsAreEqualForX() {
         GameObject gameObject = new GameObject('X');
 
         assertEquals(gameObject, new GameObject('X'));
     }
 
     @Test
-    void shouldCheckIfTwoGameObjectsAreNotEqual() {
+    void shouldCheckTwoGameObjectsAreEqualForO() {
         GameObject gameObject = new GameObject('O');
 
         assertEquals(gameObject, new GameObject('O'));
+    }
+
+    @Test
+    void shouldCheckIfTwoGameObjectsAreNotEqual() {
+        GameObject gameObject = new GameObject('O');
+
+        assertNotEquals(gameObject, new GameObject('X'));
     }
 }
